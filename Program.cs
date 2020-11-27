15aaf947a706651ccc12f3015627a2e108d7d6d6
Vinit Suryarao <suryarao.vinit@hotmail.com>
2020-11-27 12:45:00 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension_Method
+{
+    public static class IntExtensions
+    {
+        // Extension Method
+        // First param must same type of where this method will use
+        public static bool IsGreaterThan(this int i, int value)
+        {
+            return i > value;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i = 10;
+            bool result = i.IsGreaterThan(100);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
